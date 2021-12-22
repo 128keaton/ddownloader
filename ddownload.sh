70de17da65974455907c7228b0bb40bf5d91062e
Keaton Burleson <keaton.burleson@me.com>
2021-12-22 14:44:47 -0600

    Changes URL downloaded from

--- ddownload.sh
+++ ddownload.sh
@@ -14,7 +14,18 @@ do
   echo $day
   if [ ! -f /tmp/imgs/$day.png ]
   then
-    curl -sS https://dilbert.com/strip/$day|grep 'assets.amuniversal.com'|grep 'og:image'|cut -f 4 -d '"'|xargs curl -sS -o /tmp/imgs/$day.png
+    curl -sS --connect-timeout 5 \
+    --max-time 10 \
+    --retry 5 \
+    --retry-delay 0 \
+    --retry-max-time 40 \
+    https://dilbert.com/strip/$day|grep 'assets.amuniversal.com'|grep 'data-image'|cut -d '"' -f2 |xargs curl -sS -o /tmp/imgs/$day.png
+
+    echo https://dilbert.com/strip/$day
+    if [ ! -s /tmp/imgs/$day.png ]; then
+	echo "Comic for $day was empty. removing..."
+        rm -f /tmp/imgs/$day.png
+    fi
   fi
 
   i=$(($i+1))
